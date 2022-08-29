5c0cc47b91f655cd4742e351ccd591aa991ad039
saya <saya@saya.ac.cn>
2022-08-29 20:53:52 +0800

    增加Phoenix相关环境创建操作

--- phoenix-client/src/main/java/ac/cn/saya/phoenix/unit/Starter.java
+++ phoenix-client/src/main/java/ac/cn/saya/phoenix/unit/Starter.java
@@ -10,6 +10,11 @@ import java.util.Map;
 import java.util.Properties;
 
 /**
+ * https://gitee.com/astra_zhao/hbase-phoenix-docker/
+ * docker run -it -d -p 8765:8765 -p 60000:60000 -p 60010:60010 -p 60020:60020 -p 2181:2181 -p 60030:60030  -p 2888:2888 -p 3888:3888 iteblog/hbase-phoenix-docker
+ * echo /etc/hosts
+ * echo "127.0.0.1       localhost" > /etc/hosts
+ * echo "10.203.1.219      9df7e1446d20" >> /etc/hosts
  * 创建一个 schema
  * create schema business;
  * 查看所有的table
@@ -21,7 +26,7 @@ import java.util.Properties;
  * 插入一条数据
  * upsert into business.student values('1001','zhanghong','chengdu');
  * 查询数据
- * select * from business.student
+ * select * from business.student;
  */
 public class Starter{
 
@@ -29,9 +34,10 @@ public class Starter{
 
     public static void main(String[] args) throws Exception {
         // 项目资源的初始化
-        String url = "jdbc:phoenix:hadoop102,hadoop103,hadoop104:2181";
+        //String url = "jdbc:phoenix:hadoop102,hadoop103,hadoop104:2181";
+        String url = "jdbc:phoenix:10.203.1.219:2181";
         Properties props = new Properties();
-        props.put("phoenix.schema.isNamespaceMappingEnabled","true");
+        //props.put("phoenix.schema.isNamespaceMappingEnabled","true");
         dataSourceFactory = new PhoenixDataSourceFactory(url, props, 3, 5);
 
         StopWatch stopWatch = new StopWatch();
